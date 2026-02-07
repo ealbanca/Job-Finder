27ea7db13a9e18976b8f2f8a22d9544ddc15c4e0
ealbanca <ealbanca@byui.edu>
2026-02-06 21:35:37 -0700

    Added company route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const job = require('./job');
 
 routes.use('/', require('./swagger')); // Swagger documentation route
 routes.use('/job', job);
+routes.use('/company', require('./company'));
 
 module.exports = routes;
