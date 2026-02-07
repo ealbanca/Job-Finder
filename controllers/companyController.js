792bf80d9f3697d27dcc69e30bb2281b6fefd9f5
ealbanca <ealbanca@byui.edu>
2026-02-06 21:21:35 -0700

    Createdd company controller

---
+++ controllers/companyController.js
@@ -0,0 +1,18 @@
+const db =require('../models');
+const Company = db.Company;
+
+// Get all companies
+exports.getAllCompanies = (req, res) =>{
+}
+
+// Get a company by ID
+exports.getCompanyById = (req, res) =>{
+}
+
+// Create a new company
+exports.createCompany = (req, res) =>{
+}
+
+// Update a company by ID
+exports.updateCompanyById = (req, res) =>{
+}
