00a212bf68d5fd97cfa5f62a247e84ba1728fd70
ealbanca <ealbanca@byui.edu>
2026-02-21 21:26:09 -0700

    Updated Functions companies controller

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -18,7 +18,17 @@ exports.getAllCompanies = async (req, res) => {
 }
 
 // Get a company by ID
-exports.getCompanyById = (req, res) =>{
+exports.getCompanyById = async (req, res) =>{
+    const companyId = new ObjectId(req.params.id);
+    try {
+        const result = await mongodb.getDb().db().collection('companies').find({ _id: companyId });
+        result.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 }
 
 // Create a new company
@@ -41,5 +51,39 @@ exports.createCompany = async (req, res) => {
 }
 
 // Update a company by ID
-exports.updateCompanyById = (req, res) =>{
+exports.updateCompanyById = async (req, res) =>{
+    const companyId = new ObjectId(req.params.id);
+    const updatedCompany = {
+        companyName: req.body.companyName,
+        industry: req.body.industry,
+        companySize: req.body.companySize,
+        headquarters: req.body.headquarters,
+        foundedYear: req.body.foundedYear,
+        remoteFriendly: req.body.remoteFriendly
+    };
+    try {
+        const response = await mongodb.getDb().db().collection('companies').replaceOne({ _id: companyId }, updatedCompany);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json({ message: 'Some error occurred while updating the company.' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+// Delete a company by ID
+exports.deleteCompanyById = async (req, res) => {
+    const companyId = new ObjectId(req.params.id);
+    try {
+        const response = await mongodb.getDb().db().collection('companies').deleteOne({ _id: companyId });
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json({ message: 'Some error occurred while deleting the company.' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 }
